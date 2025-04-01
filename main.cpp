5eb501eea0273eba45bae5925251ff695a8d1d09
Dl0rd <124901949+Dl0rd@users.noreply.github.com>
2025-04-01 22:04:45 +0300

    v1.0.2

--- main.cpp
+++ main.cpp
@@ -6,116 +6,76 @@
 
 constexpr int FIELD_WIDTH  = 30;
 constexpr int FIELD_HEIGHT = 5;
-constexpr int timeMs       = 1300;
+constexpr int timeMs       = 100;
 constexpr int obstacles    = 3;
 
 std::mutex mtx;
 
-
-int update_position(int position[2], int sdvig)
+struct object
 {
-	position[0] += sdvig;
+	const char symbol[4];
+	int x;
+	int y;
+};
 
-	if(position[0] < 0) {
-		position[0] += FIELD_WIDTH;
-	};
-	if(position[0] >= FIELD_WIDTH) {
-		position[0] -= FIELD_WIDTH;
-	};
 
-	return position[0];
+int update_position(int x,int sdvig)
+{
+	x += sdvig;
+	if(x < 0) {
+		x += FIELD_WIDTH;
+	};
+	if(x >= FIELD_WIDTH) {
+		x -= FIELD_WIDTH;
+	};
+	return x;
 }
 
-void update_position(int position[2])
+int update_position(int y)
 {
-	position[1] += 1;
+	y += 1;
+	return y;
 }
 
 
 // Функция отрисовки
-void render_frame()
+void render_frame(const object object_array[obstacles+1])
 {
-	// std::string pole[(FIELD_WIDTH+3)*FIELD_HEIGHT];
 	std::cout << std::string(FIELD_WIDTH + 2, '-') << std::endl;
 	for (int i = 0; i != FIELD_HEIGHT;i++){
-		
-		for (int j = 0; j != FIELD_WIDTH+2;j++)
+		std::cout << "|";
+		for (int j = 0; j < FIELD_WIDTH;j++)
 		{
-			if (j == 0 || j == FIELD_WIDTH+1) std::cout << '|';
-			
-			
-			else std::cout << ' ';
+			std::string symbol = " ";
+			for (int k = 0; k != obstacles+1; k++ ){
+				if (object_array[k].x == j && object_array[k].y == i) symbol = object_array[k].symbol;
+			}
+			std::cout << symbol;
+
 		}
+		std::cout << "|";
 		std::cout << std::endl;
 	}
 	std::cout << std::string(FIELD_WIDTH + 2, '-') << std::endl;
-	// for (int i = 0; i <= (FIELD_WIDTH+3)*FIELD_HEIGHT; i++){
-	// 	if (i == 0 || i%31==0 || i%34==0){
-	// 		std::cout << "|";
-	// 	}
-	// 	if (i != 0 && i%32==0){
-	// 		std::cout << "\n";
-	// 	}
-	// 	std::cout << " ";
-		
-	// }
-	// std::cout << std::string(FIELD_WIDTH + 2, '-') << "\n" << "|" ; 
-	// for (int i = 0; i <= (FIELD_WIDTH+2)*FIELD_HEIGHT; i++){
-	// 	if (i%31==0 && i != 0) {
-	// 		std::cout << "|" << "\n";
-	// 	}
-	// 	else std::cout << " ";
-	// }
-
-	// std::cout << pole;
 }
 
-// 	std::cout << std::string(FIELD_WIDTH + 2, '-') << std::endl; 
-	
-// 	for(int j = 0;j <= 3;j++){
-// 		if (j != 3){
-// 			std::cout << "|";
-
-// 			for(int i = 0; i < FIELD_WIDTH; i++) {
-// 				for(int k = 0;k!=obstacles_quantity;k++){
-// 					if (obstacles_array[k][0] == i && obstacles_array[k][1] == j) std::cout << "#";
-// 					else std::cout << " ";
-// 				}
-// 			}
-			
-// 			std::cout << "|" << "\n";
-// 		} else {
-// 			std::cout << "|";
-			
-// 			for(int i = 0; i < FIELD_WIDTH; i++) {
-// 				for(int k = 0;k!=obstacles_quantity;k++){
-// 					if (obstacles_array[k][0] == i && obstacles_array[k][1] == j) std::cout << "#";
-// 				}
-// 				if(i == position_player) {
-// 					std::cout << "■";
-// 				}
-// 				else {
-// 					std::cout << " ";
-// 				}
-// 			}
-
-// 			std::cout << "|" << "\n" << std::string(FIELD_WIDTH + 2, '-') << std::endl;
-// 		}
-// 	}
-// }
-
-
-
-void draw_loop(const bool& game, const int& sdvig, int obstacles_array[obstacles][2],int obstacles_quantity)
+void draw_loop(const bool& game, const int& sdvig,object object_array[obstacles+1] )
 {
-	int position = 0; // начальная позиция квадрата
+
 	while(game) {
 		mtx.lock();
-		// std::cout << obstacles_quantity;  
-		// position = update_position(position, sdvig);
-		for (int i = 0; i != obstacles_quantity; i++)
-			update_position(obstacles_array[i]);
-		render_frame();
+		for (int i = 0; i != obstacles+1; i++){
+			if (i == 0)
+			{
+				int x = update_position(object_array[i].x,sdvig);
+				object_array[i].x = x;
+			}
+			else {
+				int y = update_position(object_array[i].y);
+				object_array[i].y = y;
+			}
+		}
+		render_frame(object_array);
 		mtx.unlock();
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(timeMs));
@@ -136,31 +96,32 @@ void handle_input(int code, int& shift)
 int main()
 {
 
-	std::srand(std::time(0));
-
 	bool game{true};
 	int shift{0}; //сдвиг
-	int obstacles_array[obstacles][2];
-	int obstacles_quantity{0};
+	object player = {"■", 0, FIELD_HEIGHT-1};
+	object p = {"#", 10, -1};
+	object object_array[obstacles+1] = {player,p};
+	int obstacles_quantity{1};
 	
-	auto functionGeneration = [&game,&obstacles_array,&obstacles_quantity]() {
-		while(game) {
-			if (obstacles_quantity != obstacles){
-				int start = 0;
-				int end = 30;
-				int x = rand() % (end - start + 1) + start;
-				mtx.lock();
-				obstacles_array[obstacles_quantity][0] = x;
-				obstacles_array[obstacles_quantity][1] = -1;
-				obstacles_quantity += 1;
-				mtx.unlock();
-				std::this_thread::sleep_for(std::chrono::milliseconds(timeMs/3));
-			}
-		};
-	};
+	// auto functionGeneration = [&game,&object_array,&obstacles_quantity]() {
+	// 	while(game) {
+	// 		if (obstacles_quantity != obstacles){
+	// 			int start = 1;
+	// 			int end = 30;
+	// 			std::srand(std::time(0));
+	// 			int x = rand() % (end - start + 1) + start;
+	// 			mtx.lock();
+	// 			object obstacles = {"#",x,0};
+	// 			object_array[obstacles_quantity] = obstacles;
+	// 			obstacles_quantity += 1;
+	// 			mtx.unlock();
+	// 			std::this_thread::sleep_for(std::chrono::milliseconds(timeMs/3));
+	// 		}
+	// 	};
+	// };
 
-	auto functionDraw = [&game,&shift,&obstacles_array,&obstacles_quantity]() {
-		draw_loop(game, shift, obstacles_array,obstacles_quantity);
+	auto functionDraw = [&game,&shift,&object_array]() {
+		draw_loop(game, shift,object_array);
 	};
 
 
@@ -179,11 +140,11 @@ int main()
 			mtx.unlock();
 		}
 	};
-	std::thread GenerationThread(functionGeneration); // поток для препятствий 
+	// std::thread GenerationThread(functionGeneration); // поток для препятствий 
 	std::thread drawThread(functionDraw); // поток для отрисовки персонажа
 	std::thread cinThread(functionCin); // поток для ввода направления движения
 
-	GenerationThread.join();
+	// GenerationThread.join();
 	drawThread.join();
 	cinThread.join();
 
